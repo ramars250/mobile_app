99cf2e9889eea9c718d25d249f7949a630456aea
mars.lee <ramars.tw@yahoo.com.tw>
2023-12-01 14:33:24 +0800

    完成班表

--- lib/schedule_view.dart
+++ lib/schedule_view.dart
@@ -1,81 +1,114 @@
 import 'package:flutter/material.dart';
 
 class ScheduleView extends StatelessWidget {
-  ScheduleView({super.key});
-
   final List<String> daysOfWeek = [
-    'SUN',
-    'MON',
-    'TUE',
-    'WED',
-    'THU',
-    'FRI',
-    'SAT'
+    'Sun',
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat'
   ];
-  final List<String> additionalRowData = [
-    '資料1',
-    '資料2',
-    '資料3',
-    '資料4',
-    '資料5',
-    '資料6',
-    '資料7'
+  final List<String> yourData = [
+    'Event 1\nEvent 1\nEvent 1\nEvent 1\nEvent 1\nEvent 1\nEvent 1\nEvent 1\nEvent 1',
+    'Event 2',
+    'Event 3',
+    'Event 4',
+    'Event 5',
+    'Event 6',
+    'Event 7',
+    'Event 8',
+    'Event 9',
+    'Event 10',
+    'Event 11',
+    'Event 12',
+    'Event 13',
+    'Event 14',
+    'Event 15',
+    'Event 16',
+    'Event 17',
+    'Event 18',
+    'Event 19',
+    'Event 20',
+    'Event 21',
+    'Event 22',
+    'Event 23',
+    'Event 24',
+    'Event 25',
+    'Event 26',
+    'Event 27',
+    'Event 28',
+    'Event 29',
+    'Event 30',
+    'Event 31',
   ];
 
-  List<List<String>> generateCalendarData(int year, int month) {
-    List<List<String>> calendarData = [];
+  ScheduleView({Key? key}) : super(key: key);
+
+  List<List<String>> getCalendarDays() {
+    DateTime now = DateTime.now();
+    int year = now.year;
+    int month = now.month;
+
+    List<List<String>> calendarDays = [];
 
     DateTime firstDayOfMonth = DateTime(year, month, 1);
 
-    int daysInMonth = DateTime(year, month + 1, 0).day;
-
-    int offset = firstDayOfMonth.weekday - 1;
-
-    int dayCounter = 1;
-
-    for (var i = 0; i < 6; i++) {
-      List<String> week = [];
-
-      for (var j = 0; j < 7; j++) {
-        if ((i == 0 && j < offset || dayCounter > daysInMonth)) {
-          week.add('');
-        } else {
-          week.add('$dayCounter');
-          dayCounter++;
-        }
-      }
-
-      calendarData.add(week);
-      if (dayCounter > daysInMonth) {
-        break;
-      }
-      // if (i == 0) {
-      //   week.addAll(List.generate(offset, (_) => ''));
-      // }
-
-      // for (var j = offset + 1;
-      //     j <= 7 && (i * 7 + j - offset) <= daysInMonth;
-      //     j++) {
-      //   DateTime currentDate = DateTime(year, month, i * 7 + j - offset);
-      //   week.add('${currentDate.day}');
-      // }
-      // while (week.length < 7) {
-      //   week.add('');
-      // }
-      // calendarData.add(week);
+    for (int i = 0; i < firstDayOfMonth.weekday % 7; i++) {
+      calendarDays.add(['', '']);
     }
-    return calendarData;
+
+    DateTime lastDayOfMonth = DateTime(year, month + 1, 0);
+    int daysInMonth = lastDayOfMonth.day;
+
+    for (int i = 1; i <= daysInMonth; i++) {
+      calendarDays.add([i.toString(), yourData[i - 1]]);
+    }
+
+    int remainingEmptySpaces = 7 - (calendarDays.length % 7);
+    if (remainingEmptySpaces != 7) {
+      for (int i = 0; i < remainingEmptySpaces; i++) {
+        calendarDays.add(['', '']);
+      }
+    }
+
+    return calendarDays;
   }
 
   @override
   Widget build(BuildContext context) {
-    DateTime now = DateTime.now();
-    int currentYear = now.year;
-    int currentMonth = now.month;
+    List<List<String>> calendarDays = getCalendarDays();
 
-    List<List<String>> calendarData =
-        generateCalendarData(currentYear, currentMonth);
-    // calendarData.add(additionalRowData);
+    // 生成日历日期的网格部分
+    List<Widget> calendarGrid = List.generate(calendarDays.length, (index) {
+      return Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              calendarDays[index][0],
+              style: const TextStyle(fontSize: 20.0),
+            ),
+            const Divider(height: 4), // 添加间距以分隔日期和事件信息
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: Text(
+                  calendarDays[index][1],
+                  style: const TextStyle(fontSize: 16.0),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -87,79 +120,48 @@ class ScheduleView extends StatelessWidget {
         child: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Container(
-            padding: const EdgeInsets.only(top: 20.0, left: 25.0),
+            padding: const EdgeInsets.only(top: 20.0, left: 10.0, right: 10.0),
             height: MediaQuery.of(context).size.height * 2,
             width: MediaQuery.of(context).size.width * 2,
-            alignment: Alignment.topLeft,
-            child: DataTable(
-              border: TableBorder.all(width: 2.0),
-              columns: daysOfWeek
-                  .map((day) => DataColumn(
-                      label: Container(
-                          alignment: Alignment.center,
-                          width: MediaQuery.of(context).size.width / 5.5,
-                          child: Text(day))))
-                  .toList(),
-              rows: calendarData.map((week) {
-                return DataRow(
-                  cells: week.map((day) {
-                    List<Widget> cells = [
-                      Center(child: Text(day)),
-                    ];
-
-                    // 添加一行分隔内容
-                    cells.add(const SizedBox(
-                      height: 40, // 调整高度以适应内容
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Divider(height: 1), // 分隔线
-                          Expanded(
+            child: Column(
+              children: [
+                // 将日期部分放置在日历上方
+                Row(
+                  children: daysOfWeek
+                      .map((day) => Expanded(
                             child: Center(
-                              child: Text('Your Data'), // 在这里放置你的数据
+                              child: Text(
+                                day,
+                                style: const TextStyle(
+                                  fontSize: 24.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ));
-
-                    return DataCell(Column(
-                      children: cells,
-                    ));
-                  }).toList(),
-                );
-              }).toList(),
-              // calendarData.map((week) {
-              //   // print(week);
-              //   return
-              //   DataRow(
-              //     cells: week.map((day) {
-              //       return DataCell(Column(
-              //         children: [
-              //           Center(child: Text(day)),
-              //         ],
-              //       ));
-              //     }).toList(),
-              // calendarData.map((week) {
-              //   return DataRow(
-              //     cells: week.map((day) {
-              //       List<Widget> cells = [
-              //         Center(
-              //           child: Text(day),
-              //         )
-              //       ];
-              //       cells.add(Container(
-              //         height: MediaQuery.of(context).size.height / 4,
-              //         child: Center(child: Text('Data')),
-              //       ));
-              //       return DataCell(Column(
-              //         children: cells,
-              //       ));
-              //     }).toList(),
-              //   );
-              // }).toList()
-              //   );
-              // }).toList(),
+                          ))
+                      .toList(),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 2.0),
+                  ),
+                  child: GridView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: calendarGrid.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 7,
+                      childAspectRatio: .6,
+                      crossAxisSpacing: 1.0,
+                      mainAxisSpacing: 1.0,
+                    ),
+                    itemBuilder: (BuildContext context, int index) {
+                      return calendarGrid[index];
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
         ),
@@ -167,3 +169,125 @@ class ScheduleView extends StatelessWidget {
     );
   }
 }
+
+// class ScheduleView extends StatelessWidget {
+//   ScheduleView({super.key});
+
+//   final List<String> daysOfWeek = [
+//     'SUN',
+//     'MON',
+//     'TUE',
+//     'WED',
+//     'THU',
+//     'FRI',
+//     'SAT'
+//   ];
+//   final List<String> additionalRowData = [
+//     '資料1',
+//     '資料2',
+//     '資料3',
+//     '資料4',
+//     '資料5',
+//     '資料6',
+//     '資料7'
+//   ];
+
+//   List<List<String>> generateCalendarData(int year, int month) {
+//     List<List<String>> calendarData = [];
+
+//     DateTime firstDayOfMonth = DateTime(year, month, 1);
+
+//     int daysInMonth = DateTime(year, month + 1, 0).day;
+
+//     int offset = firstDayOfMonth.weekday - 1;
+
+//     int dayCounter = 1;
+
+//     for (var i = 0; i < 6; i++) {
+//       List<String> week = [];
+
+//       for (var j = 0; j < 7; j++) {
+//         if ((i == 0 && j < offset || dayCounter > daysInMonth)) {
+//           week.add('');
+//         } else {
+//           week.add('$dayCounter');
+//           dayCounter++;
+//         }
+//       }
+
+//       calendarData.add(week);
+//       if (dayCounter > daysInMonth) {
+//         break;
+//       }
+//     }
+//     return calendarData;
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     DateTime now = DateTime.now();
+//     int currentYear = now.year;
+//     int currentMonth = now.month;
+
+//     List<List<String>> calendarData =
+//         generateCalendarData(currentYear, currentMonth);
+//     return Scaffold(
+//       appBar: AppBar(
+//         centerTitle: true,
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: const Text('部門班表'),
+//       ),
+//       body: SingleChildScrollView(
+//         scrollDirection: Axis.vertical,
+//         child: SingleChildScrollView(
+//           scrollDirection: Axis.horizontal,
+//           child: Container(
+//             padding: const EdgeInsets.only(top: 20.0, left: 25.0),
+//             height: MediaQuery.of(context).size.height * 2,
+//             width: MediaQuery.of(context).size.width * 2,
+//             alignment: Alignment.topLeft,
+//             child: DataTable(
+//               border: TableBorder.all(width: 2.0),
+//               columns: daysOfWeek
+//                   .map((day) => DataColumn(
+//                       label: Container(
+//                           alignment: Alignment.center,
+//                           width: MediaQuery.of(context).size.width / 5.5,
+//                           child: Text(day))))
+//                   .toList(),
+//               rows: calendarData.map((week) {
+//                 return DataRow(
+//                   cells: week.map((day) {
+//                     List<Widget> cells = [
+//                       Center(child: Text(day)),
+//                     ];
+
+//                     // 添加一行分隔内容
+//                     cells.add(const SizedBox(
+//                       height: 20, // 调整高度以适应内容
+//                       child: Column(
+//                         mainAxisAlignment: MainAxisAlignment.center,
+//                         children: [
+//                           Divider(height: 1), // 分隔线
+//                           Expanded(
+//                             child: Center(
+//                               child: Text('Your Data'), // 在这里放置你的数据
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     ));
+
+//                     return DataCell(Column(
+//                       children: cells,
+//                     ));
+//                   }).toList(),
+//                 );
+//               }).toList(),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
